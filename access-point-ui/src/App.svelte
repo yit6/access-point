bfb6e12bd8e998b66c77014760abe414a4859ca8
Int3gerunderflow <canidiansnowman@gmail.com>
2024-11-02 16:28:09 -0400

    initial frontend setup plus adding in icons

--- access-point-ui/src/App.svelte
+++ access-point-ui/src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Counter from './lib/Counter.svelte'
+  import Homepage from './Homepage.svelte';
   async function get_message() {
     let res = await fetch("backend-msg");
     let message = await res.text();
@@ -17,4 +18,5 @@
         {:then message}
                 <p>{message}</p>
         {/await}
+        <Homepage/>
 </main>
