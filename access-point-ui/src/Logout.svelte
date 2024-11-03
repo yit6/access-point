5f88cc1db469b7fda5bd35db3af21bf8e5a820ce
Sam <33876901+Xinovaz@users.noreply.github.com>
2024-11-03 10:40:11 -0500

    tailwind pretty

--- access-point-ui/src/Logout.svelte
+++ access-point-ui/src/Logout.svelte
@@ -1,8 +1,13 @@
 <script>
+import "./tw.css";
 let logout = () => {
 	document.cookie = "username=; SameSite=None; Secure; path=/";
 	window.location.href="/login/index.html";
 }
 </script>
 
-<button onclick={logout}>Logout!</button>
+<div class="h-screen bg-gradient-to-b from-white to-indigo-600">
+
+<button class="bg-violet-100 border-violet-600 p-4 m-4 rounded-lg font-bold text-slate-700" onclick={logout}>Logout!</button>
+
+</div>
