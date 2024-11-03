25457282da7833ed73e23faa9a39f71e386d304e
yit6 <thatsjustwyatt@gmail.com>
2024-11-03 06:39:03 -0500

    user ap page

--- access-point-ui/src/UserAPS.svelte
+++ access-point-ui/src/UserAPS.svelte
@@ -1,15 +1,28 @@
 <script>
 import APCard from './APCard.svelte';
-let aps = [
-	{name:"Wheelchair ramp",type:"ramp",status:"working"},
-	{name:"Elevator",type:"elevator",status:"not working"},
-];
+import { onMount } from 'svelte';
+let useraps = $state([ ]);
+let username = "yit";
+
+onMount(async () => {
+	let res = await fetch(`/user/${username}`);
+	let user = await res.json();
+	user.access_points.forEach(async (id) => {
+		let res = await fetch(`/ap/${id}`);
+		let ap = await res.json();
+		console.log(ap);
+		useraps.push(ap);
+		console.log(useraps);
+	});
+});
 </script>
 
 <ul>
-	{#each aps as ap}
+	{#each useraps as ap}
 	<li>
 		<APCard access_point={ap}/>
 	</li>
+	{:else}
+	<li><p>No access points yet</p></li>
 	{/each}
 </ul>
