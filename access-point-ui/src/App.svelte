acb388bfb40e701791a1a89bbfc7049c2d10a331
yit6 <thatsjustwyatt@gmail.com>
2024-11-03 09:23:08 -0500

    login/logout

--- access-point-ui/src/App.svelte
+++ access-point-ui/src/App.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
-  import Counter from './lib/Counter.svelte'
-  import Homemap from './Homemap.svelte';
-    import BottomBar from './BottomBar.svelte';
-  async function get_message() {
-    let res = await fetch("backend-msg");
-    let message = await res.text();
-    return message;
-  }
-  let promise = get_message();
+import Counter from './lib/Counter.svelte'
+import Homemap from './Homemap.svelte';
+import BottomBar from './BottomBar.svelte';
+
+let name = document.cookie.split(";").find((row)=>row.trim().startsWith("username="));
+console.log(name);
+if (name == undefined || name.split('=').length == 1) {
+	window.location.href="/login/index.html";
+} else {
+	name = name.split('=')[1];
+}
+
+async function get_message() {
+	let res = await fetch("backend-msg");
+	let message = await res.text();
+	return message;
+}
+let promise = get_message();
 </script>
 
 <main>  
---
+++ access-point-ui/src/Login.svelte
@@ -0,0 +1,25 @@
+<script>
+let username = $state("");
+let password = $state("");
+
+let login = () => {
+	fetch('/user/', {method:'POST', body:JSON.stringify({username:username, password:password})}).then(() => {
+		document.cookie = `username=${username}; SameSite=None; Secure; path=/`;
+		window.location.href="/";
+	});
+}
+</script>
+
+<form>
+	<label for="username">Username:</label>
+	<input name="username" type="text" bind:value={username}>
+
+	<br>
+
+	<label for="password">Password:</label>
+	<input name="password" type="password" bind:value={password}>
+
+	<br>
+</form>
+
+<button onclick={login}>Log in!</button>
