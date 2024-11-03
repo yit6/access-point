3e50cd1bca8b3b6cf4ce5ea4ff3f8197e55037eb
Sam <33876901+Xinovaz@users.noreply.github.com>
2024-11-02 23:13:43 -0400

    Update ap.rs

--- access-point-api/src/ap.rs
+++ access-point-api/src/ap.rs
@@ -177,7 +177,6 @@ impl AccessPoints {
 
 	fn next_id(&self) -> APID {
 		let points = Arc::clone(&self.points);
-		let id = self.next_id();
 		let points = points.lock().unwrap();
 		if points.keys().len() == 0 {
 			0
@@ -188,6 +187,7 @@ impl AccessPoints {
 
 	pub fn create_from_lat_long(&self, lat: f32, long: f32) -> AccessPoint {
 		let points = Arc::clone(&self.points);
+		let id = self.next_id();
 		let mut points = points.lock().unwrap();
 		let access_point = AccessPoint::from_lat_long(lat, long);
 		points.insert(self.next_id(), access_point.clone());
